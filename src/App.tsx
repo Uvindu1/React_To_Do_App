50d0275c3f10dc37161166ef6d1edd175c0be18f
Uvindu1 <uvinducs153@gmail.com>
2022-01-05 13:25:19 +0530

    create react to do app

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TodoForm from './components/TodoForm';
+
+function App() {
+  return (
+    <div className="todo-app">
+      <TodoForm />
+    </div>
+  );
+}
+
+export default App;
